a65170cbc714d60352c29da7c6848154a51e19d8
Darnok00 <56363711+Darnok00@users.noreply.github.com>
2022-03-28 15:39:43 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSZD97tuSLx4PZMYfBYUEhBxqOK-69Dp8",
-  authDomain: "cv-itp-forms.firebaseapp.com",
-  projectId: "cv-itp-forms",
-  storageBucket: "cv-itp-forms.appspot.com",
-  messagingSenderId: "763481193413",
-  appId: "1:763481193413:web:deaa97fddcad6490dbe9ac",
-  measurementId: "G-7E2DD2WPZL"
+  //Your Configurations
 };
 
 export const app = initializeApp(firebaseConfig,{
